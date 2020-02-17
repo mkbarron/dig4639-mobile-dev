a636c29f7f39d72c17eefc0d75aaa042a49c05b5
Michelle Barron <mi082796@ucf.edu>
2020-02-17 10:08:33 -0500

    ICE-7

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,3 +1,25 @@
+//Produces the sum of the two parameters
+/**
+ * The two parameters must be numbers, otherwise return undefined
+ * @param a The first number 
+ * @param b The second number
+ */
 export function Sum (a, b) {
-  return a + b
+if(typeof a == 'number' && typeof b == 'number')
+  return (a + b)
+else
+  return undefined;
+}
+
+export function AddList(list) {
+  return 0;
+}
+
+let op1 = "ten";
+let op2 = 2;
+let result = Sum(op1,op2);
+if(result==op1+op2) {
+  console.log("It worked!");
+} else {
+  console.log("Expected" + op1+op2 )
 }
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,9 +1,28 @@
-import { Sum } from './sum.js'
+import { Sum, AddList } from './MyMath.js'
+
+describe('Testing AddList', () => {
+  it('returns the sum of a list of numbers', () =>
+  {
+    let list = [1,2,3,4];
+    let result = AddList(list);
+    expect(result).toBe(10);
+  })
+})
+
+describe('Testing Sum', () => {
+  it('Adds a string and a number', () => {
+    var result =Sum("Test", 5);
+    expect(result).toBeUndefined();
+});
 
 test('adds 1 + 2 to equal 3', () => {
   expect(Sum(1, 2)).toBe(3)
 })
-
+test('Border cases', () => {
+  expect(Sum(-10,5)).toBe(-5)
+  expect(Sum(.1,.2)).toBe(.3)
+  expect(Sum(10000000000000000, 10000000000000000)).toBe(20000000000000000);
+})
 // ICE 
 
 // Examples for Null
